33b6d2a0913e58daac3f832290c7eab52af80402
Scharif47 <scharif47@outlook.de>
2026-02-18 22:02:59 +0100

    navbar edit and custom btn added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,42 +1,52 @@
-import {Box} from "lucide-react";
+import { Box } from "lucide-react";
+import Button from "./ui/Button";
 
 const Navbar = () => {
-    const handleAuthClick = async () => {
+  const isSignedIn = false;
+  const username = "Zuna";
 
-    };
+  const handleAuthClick = async () => {};
 
-    return (
-        <header className="navbar">
-            <nav className="inner">
-                <div className="left">
-                    <div className="brand">
-                        <Box className="logo" />
+  return (
+    <header className="navbar">
+      <nav className="inner">
+        <div className="left">
+          <div className="brand">
+            <Box className="logo" />
 
-                        <span className="name">
-                            Roomify
-                        </span>
-                    </div>
+            <span className="name">Roomify</span>
+          </div>
 
-                    <ul className="links">
-                        <a href="#">Product</a>
-                        <a href="#">Pricing</a>
-                        <a href="#">Community</a>
-                        <a href="#">Enterprise</a>
-                    </ul>
-                </div>
+          <ul className="links">
+            <a href="#">Product</a>
+            <a href="#">Pricing</a>
+            <a href="#">Community</a>
+            <a href="#">Enterprise</a>
+          </ul>
+        </div>
 
-                <div className="actions">
-                    <button onClick={handleAuthClick} className="login">
-                        Log in
-                    </button>
-
-                    <a href="#upload" className="cta">
-                        Get Started
-                    </a>
-                </div>
-            </nav>
-        </header>
-    );
+        <div className="actions">
+          {isSignedIn ? (
+            <>
+              <span className="greeting">{username ? `Hi, ${username}` : "Signed in"}</span>
+              <Button size="sm" onClick={handleAuthClick} className="login">
+                Log out
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button onClick={handleAuthClick} size="sm" variant="ghost">
+                Log in
+              </Button>
+              <a href="#upload" className="cta">
+                Get Started
+              </a>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
